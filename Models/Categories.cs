8a23ae756a1ec98741c091638d79003490c6f097
Andrew Robilliard <robilliardnz@gmail.com>
2019-01-06 22:46:50 +1300

    Generate models from PostgreSQL db

---
+++ Models/Categories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace split_api.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            TransactionParties = new HashSet<TransactionParties>();
+            Transactions = new HashSet<Transactions>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string CategoryType { get; set; }
+
+        public ICollection<TransactionParties> TransactionParties { get; set; }
+        public ICollection<Transactions> Transactions { get; set; }
+    }
+}
---
+++ Models/Transactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace split_api.Models
+{
+    public partial class Transactions
+    {
+        public Guid Id { get; set; }
+        public Guid? Category { get; set; }
+        public Guid? TransactionParty { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsShared { get; set; }
+        public DateTime Date { get; set; }
+        public Guid? AccountOut { get; set; }
+        public Guid? AccountIn { get; set; }
+
+        public Accounts AccountInNavigation { get; set; }
+        public Accounts AccountOutNavigation { get; set; }
+        public Categories CategoryNavigation { get; set; }
+        public TransactionParties TransactionPartyNavigation { get; set; }
+    }
+}
