a3ffc3f1f5b86244842df57063b9c95cbd62c26b
Andrew Robilliard <robilliardnz@gmail.com>
2019-01-07 20:59:58 +1300

    Fix model naming

---
+++ Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace split_api.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            TransactionAccountInNavigation = new HashSet<Transaction>();
+            TransactionAccountOutNavigation = new HashSet<Transaction>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Transaction> TransactionAccountInNavigation { get; set; }
+        public ICollection<Transaction> TransactionAccountOutNavigation { get; set; }
+    }
+}
