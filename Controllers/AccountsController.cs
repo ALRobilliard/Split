d87b364d2c13a77b0468d4def27339cfd5449825
Andrew Robilliard <robilliardnz@gmail.com>
2019-01-10 21:20:40 +1300

    Add Create action to AccountsController

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc; 
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic; 
 using System.Linq; 
 using System;
+using System.Threading.Tasks;
 using SplitApi.Models;  
 
 namespace SplitApi.Controllers
@@ -20,7 +22,7 @@ namespace SplitApi.Controllers
 			{
 				Account sampleAccount = new Account
 				{
-					Id = new Guid(),
+					Id = Guid.NewGuid(),
 					Name = "Sample Account"
 				};
 
@@ -29,10 +31,38 @@ namespace SplitApi.Controllers
 			}
 		}
 
+		// GET: api/Accounts
 		[HttpGet]
-		public ActionResult<List<Account>> GetAll()
+		public async Task<ActionResult<List<Account>>> GetAccounts()
 		{
-			return _context.Accounts.ToList();
+			return await _context.Accounts.ToListAsync();
+		}
+
+		// GET: api/Accounts/92b5bf46-6697-44b9-b1a4-33e6d09e9c6f
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Account>> GetAccount(Guid id)
+		{
+			Account account = await _context.Accounts.FindAsync(id);
+
+			if (account == null)
+			{
+				return NotFound();
+			}
+
+			return account;
+		}
+
+		// POST: api/Accounts
+		[HttpPost]
+		public async Task<ActionResult<Account>> PostAccount(Account account)
+		{
+			// Overwrite the ID with a new GUID for the Account to be inserted.
+			account.Id = Guid.NewGuid();
+
+			_context.Accounts.Add(account);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetAccount", new { Id = account.Id }, account);
 		}
   	}
 }
