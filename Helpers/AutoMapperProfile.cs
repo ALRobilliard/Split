9960b8417eee8c66379a6c3a4712e88cf7cf9828
Andrew Robilliard <robilliardnz@gmail.com>
2019-02-24 23:18:20 +1300

    Add helpers

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using SplitApi.Dtos;
+using SplitApi.Models;
+
+namespace SplitApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
