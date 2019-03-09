5e5ef6c9f3e5f508010ef86a4ce4180b47bb28d8
Andrew Robilliard <robilliardnz@gmail.com>
2019-03-09 22:19:31 +1300

    Controller cleanup

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,99 +1,84 @@
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic; 
-using System.Linq; 
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using System.Threading.Tasks;
-using SplitApi.Models;  
+using SplitApi.Models;
 
 namespace SplitApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-  	public class AccountsController : ControllerBase
-  	{
-		private readonly SplitContext _context;
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AccountsController : ControllerBase
+  {
+    private readonly SplitContext _context;
 
-		public AccountsController (SplitContext context)
-		{
-			_context = context;
+    public AccountsController(SplitContext context)
+    {
+      _context = context;
+    }
 
-			if (_context.Accounts.Count() == 0)
-			{
-				Account sampleAccount = new Account
-				{
-					Id = Guid.NewGuid(),
-					Name = "Sample Account"
-				};
+    // GET: api/Accounts
+    [HttpGet]
+    public async Task<ActionResult<List<Account>>> GetAccounts()
+    {
+      return await _context.Accounts.ToListAsync();
+    }
 
-				_context.Accounts.Add(sampleAccount);
-				_context.SaveChanges();
-			}
-		}
+    // GET: api/Accounts/00000000-0000-0000-0000-000000000000
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Account>> GetAccount(Guid id)
+    {
+      Account account = await _context.Accounts.FindAsync(id);
 
-		// GET: api/Accounts
-		[HttpGet]
-		public async Task<ActionResult<List<Account>>> GetAccounts()
-		{
-			return await _context.Accounts.ToListAsync();
-		}
+      if (account == null)
+      {
+        return NotFound();
+      }
 
-		// GET: api/Accounts/00000000-0000-0000-0000-000000000000
-		[HttpGet("{id}")]
-		public async Task<ActionResult<Account>> GetAccount(Guid id)
-		{
-			Account account = await _context.Accounts.FindAsync(id);
+      return account;
+    }
 
-			if (account == null)
-			{
-				return NotFound();
-			}
+    // POST: api/Accounts
+    [HttpPost]
+    public async Task<ActionResult<Account>> PostAccount(Account account)
+    {
+      _context.Accounts.Add(account);
+      await _context.SaveChangesAsync();
 
-			return account;
-		}
+      return CreatedAtAction("GetAccount", new { Id = account.AccountId }, account);
+    }
 
-		// POST: api/Accounts
-		[HttpPost]
-		public async Task<ActionResult<Account>> PostAccount(Account account)
-		{
-			// Overwrite the ID with a new GUID for the Account to be inserted.
-			account.Id = Guid.NewGuid();
+    // PUT: api/Accounts/00000000-0000-0000-0000-000000000000
+    [HttpPut("{id}")]
+    public async Task<ActionResult> PutAccount(Guid id, Account account)
+    {
+      if (id != account.AccountId)
+      {
+        return BadRequest();
+      }
 
-			_context.Accounts.Add(account);
-			await _context.SaveChangesAsync();
+      _context.Entry(account).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
 
-			return CreatedAtAction("GetAccount", new { Id = account.Id }, account);
-		}
+      return NoContent();
+    }
 
-		// PUT: api/Accounts/00000000-0000-0000-0000-000000000000
-		[HttpPut("{id}")]
-		public async Task<ActionResult> PutAccount(Guid id, Account account)
-		{
-			if (id != account.Id)
-			{
-				return BadRequest();
-			}
+    // DELETE: api/Accounts/00000000-0000-0000-0000-000000000000
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Account>> DeleteAccount(Guid id)
+    {
+      Account account = await _context.Accounts.FindAsync(id);
+      if (account == null)
+      {
+        return NotFound();
+      }
 
-			_context.Entry(account).State = EntityState.Modified;
-			await _context.SaveChangesAsync();
+      _context.Accounts.Remove(account);
+      await _context.SaveChangesAsync();
 
-			return NoContent();
-		}
-
-		// DELETE: api/Accounts/00000000-0000-0000-0000-000000000000
-		[HttpDelete("{id}")]
-		public async Task<ActionResult<Account>> DeleteAccount(Guid id)
-		{
-			Account account = await _context.Accounts.FindAsync(id);
-			if (account == null)
-			{
-				return NotFound();
-			}
-
-			_context.Accounts.Remove(account);
-			await _context.SaveChangesAsync();
-
-			return account;
-		}
-  	}
+      return account;
+    }
+  }
 }
--- Controllers/TransactionPartiesController.cs
+++ Controllers/TransactionPartiesController.cs
@@ -1,38 +1,26 @@
-using Microsoft.AspNetCore.Mvc; 
-using System.Collections.Generic; 
-using System.Linq; 
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System;
-using SplitApi.Models;  
+using SplitApi.Models;
 
-namespace SplitApi.Controllers 
+namespace SplitApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class TransactionPartiesController : ControllerBase
-	{
-		private readonly SplitContext _context;
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TransactionPartiesController : ControllerBase
+  {
+    private readonly SplitContext _context;
 
-		public TransactionPartiesController(SplitContext context)
-		{
-			_context = context;
+    public TransactionPartiesController(SplitContext context)
+    {
+      _context = context;
+    }
 
-			if (_context.TransactionParties.Count() == 0)
-			{
-				TransactionParty sampleTransactionParty = new TransactionParty
-				{
-					Id = new Guid(),
-					Name = "Sample Transaction Party"
-				};
-
-				_context.TransactionParties.Add(sampleTransactionParty);
-				_context.SaveChanges();
-			}
-		}
-
-		[HttpGet]
-		public ActionResult<List<TransactionParty>> GetAll()
-		{
-			return _context.TransactionParties.ToList();
-		}
-	}
+    [HttpGet]
+    public ActionResult<List<TransactionParty>> GetAll()
+    {
+      return _context.TransactionParties.ToList();
+    }
+  }
 }
