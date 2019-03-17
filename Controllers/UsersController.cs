e5683679ecd0305dc2c4c76daf80f96dc4661f78
Andrew Robilliard <robilliardnz@gmail.com>
2019-03-17 20:24:59 +1300

    Fix UserId claim. Add Username claim.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -50,7 +50,8 @@ namespace SplitApi.Controllers
       {
         Subject = new ClaimsIdentity(new Claim[]
           {
-                    new Claim(ClaimTypes.Name, user.UserId.ToString())
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString())
           }),
         Expires = DateTime.UtcNow.AddDays(7),
         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
