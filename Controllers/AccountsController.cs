0da7758530201e95ab327c953e9d4c52257180f3
Andrew Robilliard <robilliardnz@gmail.com>
2019-03-19 22:08:27 +1300

    Revert "Move Base Controllers to Master (#5)"
    This reverts commit 4557cc7ffd523c638f93a4d1a5eef022829b619b.

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,138 +1,99 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Generic; 
+using System.Linq; 
 using System;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using SplitApi.Models;
-using SplitApi.Dtos;
-using SplitApi.Extensions;
-using AutoMapper;
+using SplitApi.Models;  
 
 namespace SplitApi.Controllers
 {
-  [Authorize]
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AccountsController : ControllerBase
-  {
-    private readonly SplitContext _context;
-    private IMapper _mapper;
+	[Route("api/[controller]")]
+	[ApiController]
+  	public class AccountsController : ControllerBase
+  	{
+		private readonly SplitContext _context;
 
-    public AccountsController(IMapper mapper, SplitContext context)
-    {
-      _mapper = mapper;
-      _context = context;
-    }
+		public AccountsController (SplitContext context)
+		{
+			_context = context;
 
-    // GET: api/Accounts
-    [HttpGet]
-    public async Task<ActionResult<List<AccountDto>>> GetAccounts()
-    {
-      List<Account> accounts = null;
+			if (_context.Accounts.Count() == 0)
+			{
+				Account sampleAccount = new Account
+				{
+					Id = Guid.NewGuid(),
+					Name = "Sample Account"
+				};
 
-      ClaimsIdentity identity = HttpContext.User.Identity as ClaimsIdentity;
-      Guid? userId = identity.GetUserId();
-      if (userId != null)
-      {
-        accounts = await _context.Account.Where(a => a.UserId.Equals(userId)).ToListAsync();
-      }
+				_context.Accounts.Add(sampleAccount);
+				_context.SaveChanges();
+			}
+		}
 
-      return _mapper.Map<List<AccountDto>>(accounts);
-    }
+		// GET: api/Accounts
+		[HttpGet]
+		public async Task<ActionResult<List<Account>>> GetAccounts()
+		{
+			return await _context.Accounts.ToListAsync();
+		}
 
-    // GET: api/Accounts/00000000-0000-0000-0000-000000000000
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AccountDto>> GetAccount(Guid id)
-    {
-      Account account = await _context.Account.FindAsync(id);
-      ClaimsIdentity identity = HttpContext.User.Identity as ClaimsIdentity;
-      Guid? userId = identity.GetUserId();
+		// GET: api/Accounts/00000000-0000-0000-0000-000000000000
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Account>> GetAccount(Guid id)
+		{
+			Account account = await _context.Accounts.FindAsync(id);
 
-      if (account == null)
-      {
-        return NotFound();
-      }
+			if (account == null)
+			{
+				return NotFound();
+			}
 
-      if (account.UserId != userId)
-      {
-        return Unauthorized();
-      }
+			return account;
+		}
 
-      return _mapper.Map<AccountDto>(account);
-    }
+		// POST: api/Accounts
+		[HttpPost]
+		public async Task<ActionResult<Account>> PostAccount(Account account)
+		{
+			// Overwrite the ID with a new GUID for the Account to be inserted.
+			account.Id = Guid.NewGuid();
 
-    // POST: api/Accounts
-    [HttpPost]
-    public async Task<ActionResult<AccountDto>> PostAccount([FromBody] string accountName)
-    {
-      ClaimsIdentity identity = HttpContext.User.Identity as ClaimsIdentity;
-      Guid? userId = identity.GetUserId();
+			_context.Accounts.Add(account);
+			await _context.SaveChangesAsync();
 
-      Account account = new Account();
-      account.AccountName = accountName;
+			return CreatedAtAction("GetAccount", new { Id = account.Id }, account);
+		}
 
-      // Add owning user.
-      account.UserId = userId.Value;
+		// PUT: api/Accounts/00000000-0000-0000-0000-000000000000
+		[HttpPut("{id}")]
+		public async Task<ActionResult> PutAccount(Guid id, Account account)
+		{
+			if (id != account.Id)
+			{
+				return BadRequest();
+			}
 
-      _context.Account.Add(account);
-      await _context.SaveChangesAsync();
+			_context.Entry(account).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
 
-      AccountDto accountDto = _mapper.Map<AccountDto>(account);
-      return CreatedAtAction("GetAccount", new { Id = account.AccountId }, accountDto);
-    }
+			return NoContent();
+		}
 
-    // PUT: api/Accounts/00000000-0000-0000-0000-000000000000
-    [HttpPut("{id}")]
-    public async Task<ActionResult> PutAccount(Guid id, AccountDto accountDto)
-    {
-      ClaimsIdentity identity = HttpContext.User.Identity as ClaimsIdentity;
-      Guid? userId = identity.GetUserId();
+		// DELETE: api/Accounts/00000000-0000-0000-0000-000000000000
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<Account>> DeleteAccount(Guid id)
+		{
+			Account account = await _context.Accounts.FindAsync(id);
+			if (account == null)
+			{
+				return NotFound();
+			}
 
-      if (id != accountDto.AccountId)
-      {
-        return BadRequest();
-      }
+			_context.Accounts.Remove(account);
+			await _context.SaveChangesAsync();
 
-      Account account = _mapper.Map<Account>(accountDto);
-
-      if (account.UserId != userId)
-      {
-        return Unauthorized();
-      }
-
-      _context.Entry(account).State = EntityState.Modified;
-      await _context.SaveChangesAsync();
-
-      return NoContent();
-    }
-
-    // DELETE: api/Accounts/00000000-0000-0000-0000-000000000000
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<Account>> DeleteAccount(Guid id)
-    {
-      ClaimsIdentity identity = HttpContext.User.Identity as ClaimsIdentity;
-      Guid? userId = identity.GetUserId();
-
-      Account account = await _context.Account.FindAsync(id);
-
-      if (account == null)
-      {
-        return NotFound();
-      }
-
-      if (account.UserId != userId)
-      {
-        return Unauthorized();
-      }
-
-      _context.Account.Remove(account);
-      await _context.SaveChangesAsync();
-
-      return account;
-    }
-  }
+			return account;
+		}
+  	}
 }
