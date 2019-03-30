61741e2e469778c2e1c3f3216bffd512a381e8ee
Andrew Robilliard <robilliardnz@gmail.com>
2019-03-31 12:38:19 +1300

    Rename Project to 'Split'. Move api to 'server' dir

---
+++ server/Models/SplitPayment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Split.Models
+{
+  public partial class SplitPayment
+  {
+    public Guid SplitPaymentId { get; set; }
+    public Guid TransactionId { get; set; }
+    public Guid PayeeId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+
+    public User Payee { get; set; }
+    public Transaction Transaction { get; set; }
+  }
+}
