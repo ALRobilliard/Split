9b599bf778d6c99812285c4d04ff285eef1ded78
Andrew Robilliard <robilliardnz@gmail.com>
2019-04-13 00:46:57 +1200

    Create AddCategory form layout

--- client/src/components/AddCategory/AddCategory.tsx
+++ client/src/components/AddCategory/AddCategory.tsx
@@ -8,7 +8,24 @@ class AddCategory extends Component {
       <div className="addCategory">
         <div className="headingDiv">
           <h1 className="mainHeading">Add New Category</h1>
-          <Link to="/categories" className="close-wrapper"><button className="button button-close"><i className="fas fa-times-circle"></i></button></Link>
+        </div>
+        <div className="form">
+          <div className="form-line">
+            <label htmlFor="categoryName">Category Name:</label>
+            <input id="categoryName" type="text"/>
+          </div>
+          <div className="form-line">
+            <label htmlFor="categoryType">Category Type:</label>
+            <select name="categoryType" id="categoryType">
+              <option value="0">Expense</option>
+              <option value="1">Income</option>
+              <option value="2">Transfer</option>
+            </select>
+          </div>
+          <div className="form-actions">
+            <Link to="/categories"><button className="button button-close">Cancel</button></Link>
+            <Link to="/categories"><button className="button button-confirm">Create</button></Link>
+          </div>
         </div>
       </div>
     )
