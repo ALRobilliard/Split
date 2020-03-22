36b99bf0494319d48ec10f0254ce1cbf520ccc5a
Andrew Robilliard <robilliardnz@gmail.com>
2020-03-22 13:32:23 +1300

    Add DELETE fetch helper

--- client/src/helpers/utils.ts
+++ client/src/helpers/utils.ts
@@ -49,3 +49,19 @@ export function getData(url = ``, authToken: string) {
   })
   .then(response => response.json());
 }
+
+export function deleteData(url = ``, authToken: string) {
+  return fetch(url, {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, cors, *same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${authToken}`
+    },
+    redirect: "follow", // manual, *follow, error
+    referrer: "no-referrer" // no-referrer, *client
+  })
+  .then(response => response.json());
+}
